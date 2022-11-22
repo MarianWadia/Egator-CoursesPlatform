2078cba5eff28745593d14701344edb60b33fde6
MarianWadia <marianwadia55@gmail.com>
2022-11-22 19:23:56 +0200

    media queries for tablet

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ faqs.forEach(faq => {
         faq.classList.toggle('open');
     })
 });
+
+const openButton = document.querySelector('#open-menu-btn');
+const closeButton = document.querySelector('#close-menu-btn');
+
+openButton.addEventListener('click', ()=>{
+    document.querySelectorAll('.nav-menu li').forEach(item =>{
+        item.style.display = "flex";
+    })
+    openButton.style.display = "none";
+    closeButton.style.display = "inline-block";
+})
+
+closeButton.addEventListener('click', ()=>{
+    document.querySelectorAll('.nav-menu li').forEach(item =>{
+        item.style.display = "none";
+    })
+    openButton.style.display = "inline-block";
+    closeButton.style.display = "none";
+})
+
